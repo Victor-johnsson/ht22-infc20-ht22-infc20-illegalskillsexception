e734cdf24b507c5d92deac3e7e25f721855a3d5f
jendam91 <je8665da-s@student.lu.se>
2022-10-05 16:03:50 +0200

    added enclosure page and animal page
    ++++

--- ProjAssignment/ProjAssignment/Animal.xaml.cs
+++ ProjAssignment/ProjAssignment/Animal.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,53 @@ namespace ProjAssignment
     /// </summary>
     public partial class Animal : Page
     {
+        private DataAccessLayer dal = new DataAccessLayer();
         public Animal()
         {
             InitializeComponent();
+            animalTable.ItemsSource = dal.ReadByStoredProcedure("usp_ReadAnimal").DefaultView;
+            enclosureComboBox.ItemsSource = dal.ReadByStoredProcedure("usp_ReadEnclosure").DefaultView;
+            foodComboBox.ItemsSource = dal.ReadByStoredProcedure("usp_ReadFood").DefaultView;
         }
+
+        private void animalTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void enclosureComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void OnAddButtonClick(object sender, RoutedEventArgs e)
+        {
+            var name = animalNameTextBox.Text;
+            var food = foodComboBox.SelectedValue;
+            var enclosure = enclosureComboBox.SelectedValue;
+
+            var type = animalTypeTextBox.Text;
+            var foodAmount = foodIntegerUpDown.Value;
+
+            dal.CallProcedureWithParameters(
+                new[] { "@name", "@foodId", "@enclosureId", "@type", "@foodAmount" },
+                new object[] { name, food, enclosure, type, foodAmount },
+                "usp_CreateAnimal");
+            animalTable.ItemsSource = dal.ReadByStoredProcedure("usp_ReadAnimal").DefaultView;
+
+        }
+
     }
 }
--- ProjAssignment/ProjAssignment/Enclosure.xaml.cs
+++ ProjAssignment/ProjAssignment/Enclosure.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,13 +40,16 @@ namespace ProjAssignment
 
         }
 
-        private void onAddButton(object sender, RoutedEventArgs e)
+        private void OnAddButton(object sender, RoutedEventArgs e)
         {
 
             var name = nameTextBox.Text;
             var location = locationTextBox.Text;
-            var size = sizeTextBox.Text;
-            var capacity = myUpDownControl.Text;
+            var size = sizeUpDownControl.Value;
+            var capacity = myUpDownControl.Value;
+
+            
+
 
             var paramNames = new string[] { "@name", "@location" ,"@size" , "@maxAmountAnimals"};
             var values = new object[] { name, location, size, capacity };
@@ -54,9 +58,68 @@ namespace ProjAssignment
 
 
             enclosureTable.ItemsSource = dal.ReadByStoredProcedure("usp_ReadEnclosure").DefaultView;
+
+
+            nameTextBox.Clear();
+            locationTextBox.Clear();
+            sizeUpDownControl.Value = 0;
+            myUpDownControl.Value = 0;
+            
         }
 
         private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OnDeleteButton(object sender, RoutedEventArgs e)
+        {
+            var selectItem = enclosureTable.SelectedItem as DataRowView;
+            if(selectItem != null)
+            {
+                var id = selectItem[0];
+
+                dal.CallProcedureWithParameters(new[] { "@id" }, new[] { id }, "usp_DeleteEnclosure");
+                enclosureTable.ItemsSource = dal.ReadByStoredProcedure("usp_ReadEnclosure").DefaultView;
+            }
+          
+        }
+
+        private void OnUpdate(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            var dataGridRow = e.Row;
+            var row = dataGridRow.Item as DataRowView;
+            if(row != null)
+            {
+                object?[] itemArray = row.Row.ItemArray;
+
+
+                dal.CallProcedureWithParameters(
+                   new[] { "@id", "@size", "@location", "@maxAmountAnimals", "@name" },
+                   itemArray,
+                   "usp_UpdateEnclosure");
+                enclosureTable.ItemsSource = dal.ReadByStoredProcedure("usp_ReadEnclosure").DefaultView;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+        }
+
+        private void locationTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
